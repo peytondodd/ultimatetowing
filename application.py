41b713da82802d57ef46fbe9dd6e00678720513c
eamal27 <elias.amal27@gmail.com>
2018-03-21 16:46:16 -0400

    register function working in application.py

--- application.py
+++ application.py
@@ -1,7 +1,6 @@
-from cs50 import SQL
-import sqlite3
 from flask import Flask, flash, redirect, render_template, request, session
-from flask_session import Session
+from flask.ext.session import Session
+import sqlite3
 from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions
 from werkzeug.security import check_password_hash, generate_password_hash
@@ -30,9 +29,9 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-# Configure CS50 Library to use SQLite database
-#db = SQL("sqlite:///towing.db")
-db = sqlite3.connect("towing.db")
+# Configure SQLite database
+conn = sqlite3.connect("towing.db", check_same_thread=False)
+c = conn.cursor()
 
 
 @app.route("/register", methods=["GET", "POST"])
@@ -40,7 +39,6 @@ def register():
     """Register user"""
 
     if request.method == "POST":
-
         # check for username
         if not request.form.get("username"):
             return apology("Please enter username.")
@@ -64,15 +62,17 @@ def register():
         hash = generate_password_hash(request.form.get("password"))
         
         # insert new user into database
-        result = db.execute("""INSERT INTO users (id, username, hash) 
-                               VALUES (NULL, "{username}", "{hash}");""")
+        result = c.execute("""INSERT INTO users (username, hash) VALUES ("{username}", "{hash}");""".\
+                format(username=username, hash=hash))
 
         # if username exists
         if not result:
             return apology("Choose another username.")
 
+        conn.commit()
         # successful registration redirect
         return render_template("login.html")
+
 
     # registration redirect
     else:
